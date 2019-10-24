2fee16b97079f4db248c4dbb357a483a89835a90
Prad Somareddy <coventry8690@gmail.com>
2019-10-24 13:46:25 +0000

    3rd draft

--- index.php
+++ index.php
@@ -1,3 +1,62 @@
-<?php
-echo '<br/><br/><br/><br/><br/><br/>';
-echo 'You should be in the UK to access the tool.';
+<?php ?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>VEJ Video Manager</title>
+    <link rel="stylesheet" href="styles/avb4gjz-d.css" />
+    <link rel="stylesheet" href="styles/gone-fishing-common.css" />
+    <style>
+        body {
+            background-color: #0cbbee;
+        }
+
+        .logo {
+            background: url(images/Accenture_logo_white_f8991d.svg) no-repeat;
+        }
+
+        .cool-bg {
+            width: 246px;
+            height: 200px;
+            background: url(images/Plug_white.svg) no-repeat;
+        }
+
+        /* smaller screen */
+        @media only screen and (max-width: 768px) {
+            .content {
+                padding-top: 320px;
+            }
+        }
+    </style>
+</head>
+<body>
+    <div class="logo"></div>
+    <div class="cool-bg"></div>
+    <div class="divTop"><img src="images/logoVEJ.png" /></div>
+    <div class="header"><h3>VEJ Video Manager</h3></div>
+    <div class="verticalAlign"></div>
+    <div class="divAll">
+        
+        <div id='divBody' class="divBody">
+            <div style="text-align: left;">
+                <p>
+                    <span>
+                        The VEJ Video Manager can only be accessed from within the UK.
+                    </span>
+                </p>
+                <p>
+                    
+                </p>
+                <p style="text-align: center;">
+                    <span > We apologise for any inconvenience this may have caused.</span>
+                </p>	
+                
+            </div>
+        </div>
+        <div id='divBottom' class="divBottom">
+        </div>
+    </div>
+</body>
+</html>
